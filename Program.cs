75655c8972e7f63578fbf7a992ca6c315144a6c5
1 <1@LAPTOP-R20BC7O9>
2024-10-17 13:10:56 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using AdapterPattern;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ComputerGame cyberpunk2077 = new ComputerGame(
+            "Cyberpunk 2077",
+            PegiAgeRating.P18,
+            120,
+            8192,
+            70,
+            16,
+            6,
+            3.5
+        );
+
+        PCGame adaptedGame = new ComputerGameAdapter(cyberpunk2077);
+
+        Console.WriteLine("Название игры: " + adaptedGame.getTitle());
+
+        Console.WriteLine("Минимальный возраст: " + adaptedGame.getPegiAllowedAge());
+
+        Console.WriteLine("Является Triple-A: " + (adaptedGame.isTripleAGame() ? "Да" : "Нет"));
+
+        Requirements reqs = adaptedGame.getRequirements();
+        Console.WriteLine("Системные требования:");
+        Console.WriteLine("  Видеопамять (ГБ): " + reqs.getGpuGb());
+        Console.WriteLine("  Место на диске (ГБ): " + reqs.getHDDGb());
+        Console.WriteLine("  Оперативная память (ГБ): " + reqs.getRAMGb());
+        Console.WriteLine("  Частота процессора (ГГц): " + reqs.getCpuGhz());
+        Console.WriteLine("  Количество ядер: " + reqs.getCoresNum());
+    }
+}
